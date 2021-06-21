2bc133150e959f4169d13fe6b3907ae58ab45f3b
Arata-Kurokawa <ak.tumitaha@gmail.com>
2021-06-22 02:15:54 +0900

    csrfTokenをセットする処理

---
+++ app/controllers/CsrfToken.scala
@@ -0,0 +1,16 @@
+package controllers
+
+import javax.inject._
+import play.api.mvc._
+import play.api.libs.json.Json
+import play.filters.csrf._
+import play.filters.csrf.CSRF.Token
+
+class CsrfTokenController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
+  def index() = Action { implicit req =>
+    val csrfToken = CSRF.getToken.get.value
+    println(req.session)
+    Ok(Json.toJson(Map("csrfToken" -> csrfToken)))
+      .withCookies(Cookie("csrfToken", csrfToken, httpOnly = false))
+  }
+}
